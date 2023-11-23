3376747996eefdcc6794d77d92a31fb93287459a
Ali Gel <ali@gel.com>
2023-11-23 15:32:35 +0300

    firsa

--- src/test/java/stepdefinitions/MedunnaRoomCreationStepDefinitions.java
+++ src/test/java/stepdefinitions/MedunnaRoomCreationStepDefinitions.java
@@ -5,6 +5,6 @@ import io.cucumber.java.en.When;
 public class MedunnaRoomCreationStepDefinitions {
     @When("Clicks on items&titles button")
     public void clicks_on_items_titles_button() {
-
+        System.out.println();
     }
 }
